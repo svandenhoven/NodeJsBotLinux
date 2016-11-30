3679506642becb84064b33879820941f7a808db6
Sander van den Hoven <sandervd@microsoft.com>
2016-11-30 11:33:39 +0100

    Intital

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 'f6f0b526-ccad-4929-9bf1-5c3d7948611b',
+    appPassword: '4Z3cYLp5DENWNX878O1pvns'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Thanks for Contacting the NodeJS Bot.");
+});
